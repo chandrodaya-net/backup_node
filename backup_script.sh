7539e1fb5ec4a293b0b532a000ba5a1e5f9a02ab
DauTT <dautruongtan@gmail.com>
2021-09-18 22:01:38 +0200

    1) refactor backup_script.sh 2) Enhance main.py

--- backup_script.sh
+++ backup_script.sh
@@ -3,20 +3,24 @@ DATETIME=`date +%y%m%d-%H_%M_%S`
 SRC=$1
 DST=$2
 GIVENNAME=$3
+
+SRC_TAIL=$(dirname "$SRC") # Path to the source folder/file
+SRC_HEAD=$(basename "$SRC") # folder/file name
+
 showhelp(){
         echo "\n\n############################################"
         echo "# bkupscript.sh                            #"
         echo "############################################"
         echo "\nThis script will backup files/folders into a single compressed file and will store it in the current folder."
         echo "In order to work, this script needs the following three parameters in the listed order: "
-        echo "\t- The full path for the folder or file you want to backup."
+        echo "\t- The full absolute path for the folder or file you want to backup."
         echo "\t- The name of the Space where you want to store the backup at (not the url, just the name)."
-        echo "\t- The name for the backup file (timestamp will be added to the beginning of the filename)\n"
-        echo "Example: sh bkupscript.sh ./testdir testSpace backupdata\n"
+        echo "\t- The full absolute path name of the backup file (timestamp will be added to the beginning of the filename)\n"
+        echo "Example: sh bkupscript.sh /path/../testdir testSpace  /path1/path2/backupdata\n"
 }
 tarandzip(){
     echo "\n##### Gathering files #####\n"
-    if tar -czvf $GIVENNAME-$DATETIME.tar.gz $SRC; then
+    if tar -C $SRC_TAIL -czvf $GIVENNAME-$DATETIME.tar.gz $SRC_HEAD; then
         echo "\n##### Done gathering files #####\n"
         return 0
     else
@@ -34,9 +38,22 @@ movetoSpace(){
         return 1
     fi
 }
+
+cleanUp(){
+    echo "\n##### CLEAN UP #####\n"
+    if rm $GIVENNAME-$DATETIME.tar.gz ; then
+        echo "\n##### clean up file "$GIVENNAME"-"$DATETIME".tar.gz #####\n"
+        return 0
+    else
+        echo "\n##### Failed to clean up file #####\n"
+        return 1
+    fi
+}
+
 if [ ! -z "$GIVENNAME" ]; then
     if tarandzip; then
         movetoSpace
+	cleanUp
     else
         showhelp
     fi
--- main.py
+++ main.py
@@ -6,11 +6,12 @@ import json
 logger = create_logger(config.log_file_path, __name__ , config.log_level, True)
    
 CMD_MAP = { 'start_node': "sudo systemctl start {}".format(config.binary_node),
-        'stop_node': "sudo systemctl stop {}".format(config.binary_node),
+        'stop_node': "sudo systemctl stop {}; sleep 2s".format(config.binary_node),
         'start_alert': "sudo systemctl start {}".format(config.py_alert),
         'stop_alert': "sudo systemctl stop {}".format(config.py_alert),
         'backup_script': 'backup_script -> the script need two input: working_dir=?, source_folder=?',
         'run_backup': 'run_backup > the script need two input: working_dir=?, source_folder=?',
+        's3_download': 's3_download > the script need two input: working_dir=?, source_file=?',
         'test1': 'pwd; ls',
         'test2': 'lsmaldsa',
         'EXIT': "exit from the program"
@@ -34,6 +35,7 @@ def execute(cmd):
 
 def cmd_backup_script(working_dir, source_folder):
     cmd_go_to_wdir = "cd  {wdir}".format(wdir=working_dir)  
+    # https://www.maketecheasier.com/nohup-and-uses/
     return "{wdir}; sh {script_path} {src} chandrodaya  {bkup}".format(wdir=cmd_go_to_wdir,
                                                                         script_path=config.backup_script_path,
                                                                          src =source_folder,bkup=config.backup_name)
@@ -53,19 +55,33 @@ def repl():
         if cmd_key.lower() == 'exit':
             break
         
-        if cmd_key not in ['backup_script', 'run_backup']:
-            cmd_value = CMD_MAP.get(cmd_key, 'Invalid CMD_KEY={}! Try again.'.format(cmd_key))
-        else: 
+        cmd_value = None
+        if cmd_key in ['backup_script', 'run_backup', 's3_download']: 
             print("ENTER working_dir:")
             working_dir = input()
             
-            print("ENTER source_folder:")
-            source_folder = input()
+            print("ENTER source (folder/file):")
+            source = input()
             
-            cmd_value = cmd_backup_script(working_dir, source_folder)     
-        execute(cmd_value)
+            if cmd_key == 'run_backup':
+                run_backup(working_dir, source)
+            elif cmd_key == 's3_download':
+                cmd_value = s3_download(working_dir, source)
+            else:
+                cmd_value = cmd_backup_script(working_dir, source)   
+        else:
+            cmd_value = CMD_MAP.get(cmd_key, None)
+            if cmd_value is None:
+                logger.error('Invalid CMD_KEY={}! Try again.'.format(cmd_key))
+        
+        if cmd_value: 
+            execute(cmd_value)
         
 
+def s3_download(working_dir, source_file):
+    return "cd {wdir}; s3cmd get s3://chandrodaya/{src} {src} ; tar -xzvf {src} ; rm {src} ".format(wdir=working_dir,
+                                                                                                        src=source_file)
+    
 def run_backup(working_dir, source_folder):
     logger.info("************** START BACKUP ***********************")
     for cmd_key in ['stop_node', 'backup_script'] :
