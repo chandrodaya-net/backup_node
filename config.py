0e7a465a8d147e8dbc749d9d921cee1327f26f75
DauTT <dautruongtan@gmail.com>
2021-09-19 10:25:13 +0200

    Working copy

--- config.py
+++ config.py
@@ -1,9 +1,28 @@
-#log_file_path="/home/backup/logs/alert.log"
+##########################################################################################
+# BINARY NODE:
 binary_node = "junod"
 
+##########################################################################################
+# DYNAMIC CONFIG:
+
+# Directory where we currently store the blockchain data folder
+workspace_current = "/mnt/juno1_volume_fra1_01/workspace"
+
+# Directory where we plan to store going forward the blockchain data folder. 
+# It is also used to zip the current data folder to the digitalocean space
+workspace_new = "/mnt/juno_1_volume_fra1_02/workspace"
+
+##########################################################################################
+# STATIC CONFIG:
+
+
 py_alert = "indep_node_alarm"
-backup_name = "juno"
+
 backup_script_path="/home/dau/workspace/python/github.com/dauTT/backup/backup_script.sh"
+# space where we upload backup
+digital_ocean_space = "chandrodaya"
+full_path_source_data = "{}/.{}".format(workspace_current, binary_node)
+full_path_backup_name = "{}/{}".format(workspace_new, binary_node)
 
 log_file_path="/home/dau/workspace/python/github.com/dauTT/backup/logs/backup.log"
 log_level = "DEBUG" # DEBUG, INFO
--- main.py
+++ main.py
@@ -2,16 +2,42 @@ import subprocess
 from utils import create_logger
 import config
 import json
+import version 
  
 logger = create_logger(config.log_file_path, __name__ , config.log_level, True)
-   
+
+def cmd_backup_script():
+    return "sh {script_path} {src} {space}  {bkup}".format(script_path=config.backup_script_path,
+                                                           space=config.digital_ocean_space,
+                                                           src =config.full_path_source_data,
+                                                           bkup=config.full_path_backup_name)
+
+def s3_download(source_file):
+    return "cd {wdir}; s3cmd get s3://{space}/{src} {src} ; tar -xzvf {src} ; rm {src} ".format(wdir=config.workspace_new,
+                                                                                                    space=config.digital_ocean_space,
+                                                                                                    src =source_file)
+
+def run_backup():
+    logger.info("************** START BACKUP ***********************")
+    for cmd_key in ['stop_node', 'backup_script'] :
+        if cmd_key == 'backup_script':
+            cmd_value = cmd_backup_script()
+        else:
+            cmd_value = CMD_MAP[cmd_key]
+            
+        result = execute(cmd_value)
+        if result != 0 :
+            logger.info("************** BACKUP FAILED! ***********************")
+            break 
+    logger.info("************** END BACKUP ***********************")
+    
 CMD_MAP = { 'start_node': "sudo systemctl start {}".format(config.binary_node),
         'stop_node': "sudo systemctl stop {}; sleep 2s".format(config.binary_node),
         'start_alert': "sudo systemctl start {}".format(config.py_alert),
         'stop_alert': "sudo systemctl stop {}".format(config.py_alert),
-        'backup_script': 'backup_script -> the script need two input: working_dir=?, source_folder=?',
-        'run_backup': 'run_backup > the script need two input: working_dir=?, source_folder=?',
-        's3_download': 's3_download > the script need two input: working_dir=?, source_file=?',
+        'backup_script': cmd_backup_script(),
+        'run_backup': 'stop_node; backup_script',
+        's3_download': s3_download("source_file?"),
         'test1': 'pwd; ls',
         'test2': 'lsmaldsa',
         'EXIT': "exit from the program"
@@ -33,42 +59,25 @@ def execute(cmd):
         return (exc.returncode) 
 
 
-def cmd_backup_script(working_dir, source_folder):
-    cmd_go_to_wdir = "cd  {wdir}".format(wdir=working_dir)  
-    # https://www.maketecheasier.com/nohup-and-uses/
-    return "{wdir}; sh {script_path} {src} chandrodaya  {bkup}".format(wdir=cmd_go_to_wdir,
-                                                                        script_path=config.backup_script_path,
-                                                                         src =source_folder,bkup=config.backup_name)
-    
-
 def repl():
     while True:
-        logger.info("\n********** START CMD ***************\n" )
+        logger.info("\n********** START CMD: version={}***************\n".format(version.number))
         
         print(json.dumps(CMD_MAP, sort_keys=False, indent=4))
        
         print("\nENTER A CMD_KEY:")
          
         cmd_key = input()
-        
-        logger.info("\n {}".format(cmd_key))
         if cmd_key.lower() == 'exit':
             break
         
         cmd_value = None
-        if cmd_key in ['backup_script', 'run_backup', 's3_download']: 
-            print("ENTER working_dir:")
-            working_dir = input()
-            
-            print("ENTER source (folder/file):")
-            source = input()
-            
-            if cmd_key == 'run_backup':
-                run_backup(working_dir, source)
-            elif cmd_key == 's3_download':
-                cmd_value = s3_download(working_dir, source)
-            else:
-                cmd_value = cmd_backup_script(working_dir, source)   
+        if cmd_key == 's3_download':
+            print("ENTER source_file:")
+            source_file = input()
+            cmd_value = s3_download(source_file)
+        elif cmd_key == 'run_backup':
+            run_backup()
         else:
             cmd_value = CMD_MAP.get(cmd_key, None)
             if cmd_value is None:
@@ -76,26 +85,9 @@ def repl():
         
         if cmd_value: 
             execute(cmd_value)
-        
-
-def s3_download(working_dir, source_file):
-    return "cd {wdir}; s3cmd get s3://chandrodaya/{src} {src} ; tar -xzvf {src} ; rm {src} ".format(wdir=working_dir,
-                                                                                                        src=source_file)
-    
-def run_backup(working_dir, source_folder):
-    logger.info("************** START BACKUP ***********************")
-    for cmd_key in ['stop_node', 'backup_script'] :
-        if cmd_key == 'backup_script':
-            cmd_value = cmd_backup_script(working_dir, source_folder)
-        else:
-            cmd_value = CMD_MAP[cmd_key]
-            
-        result = execute(cmd_value)
-        if result != 0 :
-            logger.info("************** BACKUP FAILED! ***********************")
-            break 
-    logger.info("************** END BACKUP ***********************")
-        
+                
 
 if __name__ == "__main__":
     repl()
+    
+    
