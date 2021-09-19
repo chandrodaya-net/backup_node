d3908e4ade09d96dbeddfe1c31a7d667cc4cb5ff
DauTT <dautruongtan@gmail.com>
2021-09-19 10:48:06 +0200

    Follow template config

--- config_juno.py
+++ config_juno.py
@@ -1,9 +1,29 @@
-#log_file_path="/home/backup/logs/alert.log"
+##########################################################################################
+# BINARY NODE:
 binary_node = "junod"
 
-py_alert = "indep_node_alarm"
-backup_name = "juno"
-backup_script_path="/home/dau/workspace/python/github.com/dauTT/backup/backup_script.sh"
+##########################################################################################
+# DYNAMIC CONFIG:
 
-log_file_path="/home/dau/workspace/python/github.com/dauTT/backup/logs/backup.log"
+# Directory where we currently store the blockchain data folder
+workspace_current = "/mnt/juno1_volume_fra1_01/workspace"
+
+# Directory where we plan to store going forward the blockchain data folder. 
+# It is also used to zip the current data folder to the digitalocean space
+workspace_new = "/mnt/juno_1_volume_fra1_02/workspace"
+
+
+##########################################################################################
+# STATIC CONFIG:
+
+py_alert = "indep_node_alarm"
+
+backup_script_path = "/root/py/backup/backup_script.sh"
+# space where we upload backup
+digital_ocean_space = "chandrodaya"
+full_path_source_data = "{}/.{}".format(workspace_current, binary_node[:-1])
+full_path_backup_name = "{}/{}".format(workspace_new, binary_node[:-1])
+
+log_file_path = "/root/py/backup/logs/backup.log"
 log_level = "DEBUG" # DEBUG, INFO
+
