ea0aca69042c3c051a34f205f01210548332701a
DauTT <dautruongtan@gmail.com>
2022-01-30 10:49:08 +0100

    Update config template

--- config_template/config_template.py
+++ config_template/config_template.py
@@ -1,17 +1,19 @@
 ##########################################################################################
 # DYNAMIC CONFIG:
-binary_node = "junod"
+binary_node = "noded"
 volume_current = '/mnt/volume_fra1_01'
 volume_new = '/mnt/volume_fra1_02'
 ##########################################################################################
 # STATIC CONFIG:
-backup_script_path = "/root/py/backup/backup_script.sh"
+backup_script_path="/home/dau/workspace/python/github.com/dauTT/backup/backup_script.sh"
 digital_ocean_space = "chandrodaya"
 
-log_file_path = "/root/py/backup/logs/backup.log"
+log_file_path="/home/dau/workspace/python/github.com/dauTT/backup/logs/backup.log"
 log_level = "DEBUG" # DEBUG, INFO
+
 #########################################################################################
 # TELEGRAM OONFIG
-node_name = "digital-Juno-1-Prod || 137.184.64.186"
-telegram_token = "1962452788:AAFpzvsfa-MBSEzXq68C1YERLvh57jLi8hI" # bot identification
-telegram_chat_id = "-460038604" # Chandrodaya-Bots-PROD
+node_name = "digital-Juno-1 || 138.68.107.126"
+telegram_token = "" # bot identification
+telegram_chat_id = ""
+
